e6c364d549dc673c30243c69b559373f5583f3b5
avnotaklu <sukhmander111101@gmail.com>
2025-01-09 20:11:24 +0530

    Allow cors for static files

--- Startup.cs
+++ Startup.cs
@@ -106,7 +106,15 @@ public class Startup
             app.UseSwagger();
             app.UseSwaggerUI();
         }
-        app.UseStaticFiles();
+        app.UseStaticFiles(new StaticFileOptions()
+        {
+            OnPrepareResponse = ctx =>
+            {
+                ctx.Context.Response.Headers.Append("Access-Control-Allow-Origin", "*");
+                ctx.Context.Response.Headers.Append("Access-Control-Allow-Headers",
+                  "Origin, X-Requested-With, Content-Type, Accept");
+            },
+        });
 
         app.UseCors(MyAllowSpecificOrigins);
         app.UseAuthentication();
